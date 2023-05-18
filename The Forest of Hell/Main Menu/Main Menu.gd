4dae2756d4a3532369790bc7f83ec6c8bebfaf7e
R9_ <neamtrares@naver.com>
2023-05-18 22:00:18 +0300

    Player + Tree Generator
    The Generator may take from 10 to 60 seconds depending on the computer

--- The Forest of Hell/Main Menu/Main Menu.gd
+++ The Forest of Hell/Main Menu/Main Menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_Play_pressed():
-	get_tree().change_scene("res://Main Menu/Main Menu.tscn")
+	get_tree().change_scene("res://Game/Game.tscn")
 
 func _on_Exit_pressed():
 	get_tree().quit()
